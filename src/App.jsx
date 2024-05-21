c0b8c2061606fa272a1cfef018bdf8b4415af419
sangjun <987654321123sin@gmail.com>
2024-05-21 10:32:06 +0900

    ex1 완성

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,28 @@ export default function App() {
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter((student) => student.age >= 18);
+
+  const printInfo = (Info) => {
+    alert(`나이: ${Info.age} , 점수: ${Info.grade}`);
+  };
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
-        {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
-        {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
+        {filteredStudents.map((student, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => {
+                printInfo(student);
+              }}
+            >
+              {student.name}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
